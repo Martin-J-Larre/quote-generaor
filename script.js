4ddb45947125d9593e2a1e9cf5303a7edd5e43eb
Martin-J-Larre <martinjlarre@gmail.com>
2022-10-17 23:31:50 -0400

    #7 Buttons with funions and events edded

--- script.js
+++ script.js
@@ -1,15 +1,20 @@
 const quoteContainer = document.getElementById('quote-container');
-const quote = document.getElementById('quote');
-const author = document.getElementById('author');
-const twitter = document.getElementById('twitter');
+const quoteText = document.getElementById('quote');
+const quoteAuthor = document.getElementById('author');
+const twitterBtn = document.getElementById('twitter');
 const newQuoteBtn = document.getElementById('new-quote-btn');
 
 let apiQuotes = [];
 
 const newQuote = () => {
     const quote = apiQuotes[Math.floor(Math.random() * apiQuotes.length)];
-    console.log(quote);
+
+    quoteText.textContent = quote.text;
+    !quote.author ? quoteAuthor.textContent = "Unknown" : quoteAuthor.textContent = quote.author;
+
+    quote.text.length > 100 ? quoteText.classList.add('.long-quote') : quoteText.classList.remove('.long-quote')
 }
+
 const getQuote = async () => {
     const apiUrl = 'https://type.fit/api/quotes';
     try {
@@ -20,4 +25,13 @@ const getQuote = async () => {
         console.log('Error', err);
     }
 }
+
+const tweetQuote = () => {
+    const twitterURL = `https://twitter.com/intent/tweet?text=${quoteText.textContent} - ${quoteAuthor.textContent}`;
+    window.open(twitterURL, '_blank');
+}
+
+newQuoteBtn.addEventListener('click', newQuote);
+twitterBtn.addEventListener('click', tweetQuote);
+
 getQuote();
